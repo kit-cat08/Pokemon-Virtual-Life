374c17ebc82d936d446fe797d158c209e94446ed
kit-cat08 <cathyvo2118@gmail.com>
2025-12-03 10:27:27 -0800

    Create Pikachu.java

---
+++ Pikachu.java
@@ -0,0 +1,11 @@
+public class Pikachu {
+    private int health;
+    private int energy;
+    private int happiness;
+
+    public Pikachu(int theHealth, int energy, int happiness) {
+        this.health = health;
+        this.energy = energy;
+        this.happiness = happiness;
+    }
+}
