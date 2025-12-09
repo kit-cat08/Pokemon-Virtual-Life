4e8540ce50f030aa528a4e5d852d9627899542cc
sonicaidanxg-jpg <sonicaidanxg@gmail.com>
2025-12-09 10:07:19 -0800

    squirtle

--- squirtle.java
+++ squirtle.java
@@ -83,7 +83,7 @@ public class squirtle {
         }
     }
     /*
-    This method simulates Squitle playing, increasing its happiness and reducing its energy.
+    This method simulates Squitle playing, increasing its happiness and reducing its energy. fadssa
      */
     public void play(){
         System.out.println("Pikachu is playing!");
